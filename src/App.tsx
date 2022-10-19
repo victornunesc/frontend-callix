b2508277daa02b5d3fe19b7e7b79fe803ba64a6c
Victor Nunes <victornunesc95@gmail.com>
2022-10-19 19:19:27 -0300

    feat: initial settings and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <button>Próximo Lançamento</button>
+        <button>Último lançamento</button>
+        <button>Próximos lançamentos</button>
+        <button>Lançamentos passados</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
