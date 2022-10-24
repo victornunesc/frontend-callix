df29a80531c72c593e00062aac6bc63f07d88299
Victor Nunes <victornunesc95@gmail.com>
2022-10-24 12:28:41 -0300

    feat: search bar

---
+++ src/components/searchBox/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Search = styled.input`
+  height: 30px;
+  width: 206px;
+  border: 1px solid orange;
+  border-radius: 8px;
+  margin: 20px;
+  text-align: center;
+`;
