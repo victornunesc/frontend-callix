f07272911bf9108a8c92b012d184740a578db54e
Victor Nunes <victornunesc95@gmail.com>
2023-11-18 15:12:13 -0300

    refactor: change heroku to render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiSpaceX = axios.create({
-  baseURL: "https://backend-callix.herokuapp.com",
+  baseURL: "https://backend-callix.onrender.com",
 });
 
 export default apiSpaceX;
